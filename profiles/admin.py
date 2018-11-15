c641b05e275d9acbc552a083be79f8a05fadd65c
James Graham <J.Graham@software.ac.uk>
2018-11-15 11:13:01 +0000

    Add new fields to user admin page - #30

--- profiles/admin.py
+++ profiles/admin.py
@@ -4,7 +4,22 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
-admin.site.register(models.User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('organisational_unit',)
+
+    fieldsets = (
+        (None,
+            {'fields': ('username', 'password')}),
+        ('Personal info',
+            {'fields': ('first_name', 'last_name', 'email', 'organisational_unit')}),
+        ('Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_auditor', 'is_superauditor', 'groups', 'user_permissions')}),
+        ('Important dates',
+            {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(models.User, CustomUserAdmin)
 
 
 @admin.register(models.OrganisationalUnit)
