e37ae831cfd90773b6e8dd91f2ab725c1550db98
James Graham <J.Graham@software.ac.uk>
2018-11-21 11:26:54 +0000

    Add test for user token endpoint

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,9 @@
 from django.test import Client, TestCase
+from django.urls import reverse
 
-from .models import User
+from rest_framework.authtoken.models import Token
+
+from . import models
 
 
 class UserTest(TestCase):
@@ -8,7 +11,7 @@ class UserTest(TestCase):
         """
         Test that we can get a URI for a user.
         """
-        user = User.objects.create_user('Test User')
+        user = models.User.objects.create_user('Test User')
         uri = user.get_uri()
 
         self.assertEqual(str, type(uri))
@@ -27,3 +30,36 @@ class UserTest(TestCase):
         self.assertIn('user', data)
         self.assertIn('pk', data['user'])
         self.assertEqual(user.pk, data['user']['pk'])
+
+    def test_user_get_token(self):
+        """
+        Check that we can get an API Token for a given user.
+        """
+        user = models.User.objects.create_user('Test User')
+
+        client = Client()
+        client.force_login(user)
+
+        with self.assertRaises(Token.DoesNotExist):
+            # User should not already have token
+            token = Token.objects.get(user=user)
+
+        response = client.get(reverse('profiles:token'))
+
+        self.assertEqual(200, response.status_code)
+
+        content = response.json()
+        self.assertIn('status', content)
+        self.assertIn('data', content)
+        self.assertEqual('success', content['status'])
+
+        data = content['data']
+        self.assertIn('token', data)
+        self.assertIn('key', data['token'])
+
+        key = data['token']['key']
+
+        token = Token.objects.get(user=user)
+
+        self.assertEqual(key, token.key)
+
