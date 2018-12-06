58bf1676d8a4667c1b15fb3fb93d2cf0ff3e4b1b
James Graham <J.Graham@software.ac.uk>
2018-12-06 13:40:53 +0000

    Update goaccess script to include rotated logs

--- scripts/goaccess.sh
+++ scripts/goaccess.sh
@@ -5,8 +5,10 @@ if [[ "$#" -ne 1 ]]; then
     exit 1
 fi
 
-if [[ -f /var/log/nginx/access.log.1.gz ]]; then
-    zcat /var/log/nginx/access.log.*.gz | goaccess -q --log-format=COMBINED -o $1 /var/log/nginx/access.log -
+if [[ -f /var/log/nginx/access.log.2.gz ]]; then
+    zcat /var/log/nginx/access.log.*.gz | goaccess -q --log-format=COMBINED -o $1 /var/log/nginx/access.log /var/log/nginx/access.log.1 -
+elif [[ -f /var/log/nginx/access.log.1 ]]; then
+    goaccess -q --log-format=COMBINED -o $1 /var/log/nginx/access.log /var/log/nginx/access.log.1 -
 else
     goaccess -q --log-format=COMBINED -o $1 /var/log/nginx/access.log
 fi
