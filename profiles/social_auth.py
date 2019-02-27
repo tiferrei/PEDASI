c3757f5f74b4c45d8f295bee01317378f77d0b8b
James Graham <J.Graham@software.ac.uk>
2019-02-27 11:29:02 +0000

    Re-enable functionality to start new user accounts as disabled

--- profiles/social_auth.py
+++ profiles/social_auth.py
@@ -1,7 +1,18 @@
+"""
+Module containing customisations to the Python Social Auth pipeline.
+
+See https://python-social-auth.readthedocs.io/en/latest/
+"""
+
 from social_core.pipeline.user import create_user
 
 
 def create_user_disabled(strategy, details, backend, user=None, *args, **kwargs):
+    """
+    Create a user account for the user being authenticated - but mark it as disabled.
+
+    A new user must have their account enabled by an admin before they are able to log in.
+    """
     # Returns dict containing 'is_new' and 'user'
     result = create_user(strategy, details, backend, user, *args, **kwargs)
 
