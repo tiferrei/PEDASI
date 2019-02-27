90c9cc2f1fd561bab6f5d324d6e61f6813c21184
James Graham <J.Graham@software.ac.uk>
2019-02-27 17:29:03 +0000

    Add admin email to account creation process - see #65
    Need to add admin email addresses to settings

--- profiles/social_auth.py
+++ profiles/social_auth.py
@@ -4,6 +4,9 @@ Module containing customisations to the Python Social Auth pipeline.
 See https://python-social-auth.readthedocs.io/en/latest/
 """
 
+from django.core.mail import mail_admins
+
+
 from social_core.pipeline.user import create_user
 
 
@@ -22,3 +25,14 @@ def create_user_disabled(strategy, details, backend, user=None, *args, **kwargs)
         django_user.save()
 
     return result
+
+
+def email_admins(strategy, details, backend, user=None, *args, **kwargs):
+    """
+    Email the PEDASI admins if a new account has been created and requires approval
+    """
+    if kwargs['is_new']:
+        mail_admins(
+            subject='PEDASI Account Created',
+            message='User account created'
+        )
