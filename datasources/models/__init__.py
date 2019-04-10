768711e9d19bfd9494fe5649c2fc52bf9396564f
James Graham <J.Graham@software.ac.uk>
2019-04-10 12:14:32 +0100

    Refactor data source models into models package

---
+++ datasources/models/__init__.py
@@ -0,0 +1,16 @@
+"""
+This package contains the :class:`DataSource` model representing an internal or
+external data source and the models necessary to support it.
+"""
+
+from .datasource import (
+    Licence,
+    UserPermissionLevels,
+    UserPermissionLink,
+    DataSource
+)
+
+from .metadata import (
+    MetadataField,
+    MetadataItem
+)
