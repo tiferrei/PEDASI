14b7bc7974e5fa2b2eb8d02f99cbf9cd59646ff6
James Graham <J.Graham@software.ac.uk>
2019-04-10 15:56:20 +0100

    Clean up and document quality tests

--- datasources/tests/test_quality.py
+++ datasources/tests/test_quality.py
@@ -13,13 +13,26 @@ class QualityRulesetTest(TestCase):
             username='Test User 1'
         )
 
-    def test_create_ruleset(self):
-        ruleset = models.QualityRuleset.objects.create(
+        cls.metadata_field = models.MetadataField.objects.create(
+            name='Test Metadata Field 1',
+            short_name='TMF1'
+        )
+
+    def setUp(self):
+        super().setUp()
+
+        self.ruleset = models.QualityRuleset.objects.create(
             name='Test Ruleset 1',
             short_name='TR1v1',
             version='1'
         )
 
+        self.datasource = models.DataSource.objects.create(
+            name='Test Data Source 1',
+            owner=self.user
+        )
+
+    def test_create_ruleset(self):
         ruleset = models.QualityRuleset.objects.get(
             short_name='TR1v1'
         )
@@ -31,163 +44,111 @@ class QualityRulesetTest(TestCase):
         return ruleset
 
     def test_ruleset_eval_null(self):
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        datasource = models.DataSource.objects.create(
-            name='Test Data Source 1',
-            owner=self.user
-        )
-
-        level = ruleset(datasource)
+        """
+        Test that an empty ruleset identifies any datasource as level 0.
+        """
+        level = self.ruleset(self.datasource)
 
         self.assertEqual(0, level)
 
     def test_ruleset_create_level(self):
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        ruleset.levels.create(
+        """
+        Test that levels can be added to rulesets.
+        """
+        self.ruleset.levels.create(
             level=1
         )
 
-        self.assertEqual(1, ruleset.levels.count())
+        self.assertEqual(1, self.ruleset.levels.count())
 
     def test_ruleset_create_criterion(self):
-        metadata_field = models.MetadataField.objects.create(
-            name='Test Metadata Field 1',
-            short_name='TMF1'
-        )
-
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        level_1 = ruleset.levels.create(
+        """
+        Test that criteria can be added to quality levels.
+        """
+        level_1 = self.ruleset.levels.create(
             level=1
         )
 
         level_1.criteria.create(
-            metadata_field=metadata_field
+            metadata_field=self.metadata_field
         )
 
         self.assertEqual(1, level_1.criteria.count())
 
     def test_ruleset_eval_criterion(self):
-        metadata_field = models.MetadataField.objects.create(
-            name='Test Metadata Field 1',
-            short_name='TMF1'
-        )
+        """
+        Test that criteria are evaluated correctly.
 
-        datasource = models.DataSource.objects.create(
-            name='Test Data Source 1',
-            owner=self.user
-        )
-
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        level_1 = ruleset.levels.create(
+        Should return weight contributed to level threshold.
+        """
+        level_1 = self.ruleset.levels.create(
             level=1,
             threshold=1
         )
 
         criterion = level_1.criteria.create(
-            metadata_field=metadata_field,
+            metadata_field=self.metadata_field,
             weight=1
         )
 
         self.assertEqual(1, level_1.criteria.count())
 
-        self.assertAlmostEqual(0, criterion(datasource))
+        self.assertAlmostEqual(0, criterion(self.datasource))
 
-        datasource.metadata_items.create(
-            field=metadata_field,
+        self.datasource.metadata_items.create(
+            field=self.metadata_field,
             value=''
         )
 
-        self.assertAlmostEqual(1, criterion(datasource))
+        self.assertAlmostEqual(1, criterion(self.datasource))
 
     def test_ruleset_eval_level(self):
-        metadata_field = models.MetadataField.objects.create(
-            name='Test Metadata Field 1',
-            short_name='TMF1'
-        )
+        """
+        Test that quality levels are evaluated correctly.
 
-        datasource = models.DataSource.objects.create(
-            name='Test Data Source 1',
-            owner=self.user
-        )
-
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        level_1 = ruleset.levels.create(
-            level=1
-            # threshold=1
+        Should return whether data source passes level threshold.
+        """
+        level_1 = self.ruleset.levels.create(
+            level=1,
+            threshold=1
         )
 
         level_1.criteria.create(
-            metadata_field=metadata_field,
+            metadata_field=self.metadata_field,
             weight=1
         )
 
-        self.assertFalse(level_1(datasource))
+        self.assertFalse(level_1(self.datasource))
 
-        datasource.metadata_items.create(
-            field=metadata_field,
+        self.datasource.metadata_items.create(
+            field=self.metadata_field,
             value=''
         )
 
-        self.assertTrue(level_1(datasource))
+        self.assertTrue(level_1(self.datasource))
 
     def test_ruleset_eval(self):
-        metadata_field = models.MetadataField.objects.create(
-            name='Test Metadata Field 1',
-            short_name='TMF1'
-        )
+        """
+        Test that rulesets are evaluated correctly.
 
-        datasource = models.DataSource.objects.create(
-            name='Test Data Source 1',
-            owner=self.user
-        )
-
-        ruleset = models.QualityRuleset.objects.create(
-            name='Test Ruleset 1',
-            short_name='TR1v1',
-            version='1'
-        )
-
-        level_1 = ruleset.levels.create(
-            level=1
-            # threshold=1
+        Should return quality level of data source.
+        """
+        level_1 = self.ruleset.levels.create(
+            level=1,
+            threshold=1
         )
 
         level_1.criteria.create(
-            metadata_field=metadata_field,
+            metadata_field=self.metadata_field,
             weight=1
         )
 
-        self.assertEqual(0, ruleset(datasource))
+        self.assertEqual(0, self.ruleset(self.datasource))
 
-        datasource.metadata_items.create(
-            field=metadata_field,
+        self.datasource.metadata_items.create(
+            field=self.metadata_field,
             value=''
         )
 
-        self.assertEqual(1, ruleset(datasource))
+        self.assertEqual(1, self.ruleset(self.datasource))
 
